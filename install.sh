a1043808da118bfb0d35056f4d1e48d81d1520f6
Chris McGehee <cmcgehee@datto.com>
2022-01-23 17:51:33 -0800

    Install lyncser

--- install.sh
+++ install.sh
@@ -2,8 +2,14 @@
 
 echo 'Installing dotfiles...'
 mkdir -p $HOME/.config/lyncser
-# eval $(gp env -e)
 if [[ ! -z "$REAL_GCP_ACCOUNT_CREDENTIALS" ]]; then
+  if ! command -v lyncser; then
+    echo 'Installing Lyncser...'
+    git clone https://github.com/chrismcgehee/lyncser.git $HOME/.lyncser
+    cd $HOME/.lyncser
+    make build
+    sudo ln -s $HOME/.lyncser/lyncser /usr/local/bin/lyncser
+  fi
   cp $HOME/.gitconfig $HOME/.gitconfig_gitpod
   echo "$REAL_GCP_ACCOUNT_CREDENTIALS" > $HOME/.config/lyncser/credentials.json
   echo "$REAL_GCP_OAUTH_TOKEN" > $HOME/.config/lyncser/token.json
